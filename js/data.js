85d9e0cb42e13e3c302843254ff0a6305e711b3f
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-14 08:41:28 +0000

    added conditinal statement to parse if needed

--- js/data.js
+++ js/data.js
@@ -1,14 +1,20 @@
 /* exported data */
-function beforeUnloadListener(e) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('javascript-local-storage', dataJSON);
-}
-
-window.addEventListener('beforeunload', beforeUnloadListener);
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+function beforeUnloadListener(e) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
+
+var previousDataJSON = localStorage.getItem('javascript-local-storage');
+
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnloadListener);
