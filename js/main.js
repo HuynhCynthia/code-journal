77fc50f27b969db0db7ccb8715353e4811ee7c94
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-14 08:17:34 +0000

    Added a submit event listener to form element

--- js/main.js
+++ js/main.js
@@ -6,8 +6,31 @@ function inputPhotoUrl(e) {
   $imageURL.src = e.target.value;
 }
 
+function handleSubmit(e) {
+  event.preventDefault();
+
+  var formValues = {};
+
+  var $form = document.querySelector('.form-container');
+  var $title = $form.elements.title.value;
+  var $url = $form.elements.url.value;
+  var $notes = $form.elements.notes.value;
+  var $entryId = data.nextEntryId;
+
+  formValues.entryId = $entryId;
+  formValues.title = $title;
+  formValues.url = $url;
+  formValues.notes = $notes;
+
+  document.querySelector('.form-container').reset();
+  $imageURL.src = 'images/placeholder-image-square.jpg';
+  data.nextEntryId = $entryId + 1;
+}
+
 var $photoURL = document.querySelector('#photourl');
 var $imageURL = document.querySelector('.image-url');
+var $submit = document.querySelector('form');
 
 $photoURL.addEventListener('paste', pastePhotoUrl);
 $photoURL.addEventListener('input', inputPhotoUrl);
+$submit.addEventListener('submit', handleSubmit);
