a732d10572b3fa04b079636470bde5b0e3a206ec
Cynthia Huynh <115051720+synthwin@users.noreply.github.com>
2023-02-14 08:30:21 +0000

    add a beforeunload event listener

--- js/data.js
+++ js/data.js
@@ -1,4 +1,10 @@
 /* exported data */
+function beforeUnloadListener(e) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnloadListener);
 
 var data = {
   view: 'entry-form',
